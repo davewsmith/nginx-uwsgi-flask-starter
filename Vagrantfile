1b80f816325cc46a75e61e79400e37b1b4f5b5de
Dave Smith <davewsmith@gmail.com>
2017-01-02 21:25:13 -0800

    ubuntu/xenial64 has the host dir extenstions, so use it

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,7 @@
 
 Vagrant.configure(2) do |config|
-    config.vm.box = "debian/jessie64"
+    # config.vm.box = "debian/jessie64"  # alas, no guest additions in this box
+    config.vm.box = "ubuntu/xenial64"
     config.vm.network "forwarded_port", guest: 5000, host: 5000
     config.vm.network :private_network, ip: "10.10.10.10"
 
--- provision/provision.sh
+++ provision/provision.sh
@@ -3,21 +3,23 @@
 echo `pwd` >> /vagrant/whereami
 
 sudo apt-get update
-# This is pro-forma, and not necessary while playing
-#sudo apt-get -y upgrade
+# This is pro-forma, and not necessary while playing around
+#sudo apt-get upgrade -y
 
 # Basic quality-of-lift stuff
-sudo apt-get -y install vim git
+sudo apt-get install -y vim git
 
 
-sudo apt-get -y install build-essential python3-dev python3.4-dev python3-setuptools python3-pip
-sudo apt-get -y install python-virtualenv
-sudo apt-get -y install sqlite3
-
-# sudo apt-get -y install nginx
+sudo apt-get install -y build-essential python3-dev python3-setuptools python3-pip
+# sudo apt-get install -y python3.5-dev  # needed?
+sudo apt-get install -y python-virtualenv
+sudo apt-get install -y sqlite3
 
 cd /vagrant
 virtualenv venv --python=python3
 . venv/bin/activate
 pip install -r requirements.txt
+
+# sudo apt-get install -y nginx
+
 
